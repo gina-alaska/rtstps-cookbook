87de4cca5e01405b7b515a7246bcc015b2b3d0e3
Scott Macfarlane <scott@gina.alaska.edu>
2014-11-04 14:41:07 -0900

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "scott@gina.alaska.edu"
 license          "All rights reserved"
 description      "Installs/Configures rtstps"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.2.0"
+version          "2.0.0"
 
 depends "java"
 
